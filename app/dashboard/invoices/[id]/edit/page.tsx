48e035155fb0142150532466ebcc5e50e0c32f75
Maksym-Holovachuk <mgolovachuk@gmail.com>
2024-04-02 17:33:26 +0300

    finished chapter 13 - Handling Errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import Form from '@/app/ui/invoices/edit-form';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 interface IPageProps {
   params: { id: string };
@@ -13,6 +14,10 @@ const Page: FC<IPageProps> = async ({ params }) => {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
